f1b485f4986f18afa058cefb0278204705a54faf
Oscar <oscar101019@gmail.com>
2018-07-18 16:37:28 -0600

    Correcciones al Semantico

--- app/src/main/java/mx/edu/ittepic/compilador/AnalizadorSemantico.java
+++ app/src/main/java/mx/edu/ittepic/compilador/AnalizadorSemantico.java
@@ -71,10 +71,17 @@ public class AnalizadorSemantico {
                     if((temp.tipo+"").equals("ID")){
                         temp = tablaSimbolos.get(temp.valor+"");
                     }
+
+                    if(tablaSimbolos.get(aux[3].split(" ")[0]).valor!= temp.valor){
+                        String idObj = aux[3].split(" ")[1];
+                        tablaSimbolos.put(idObj,temp);
+                    }
+
                     String tipo = (String)tablaSimbolos.get(aux[3].split(" ")[0]).tipo;
-                    if(!temp.tipo.equals(tipo) && !temp.tipo.equals("ERR")){
+                    if(!auxErr(temp.tipo.toString()).equals(tipo) && !auxErr(temp.tipo.toString()).equals("ERR")){
                         err[0] += "Error semantico, en la linea "+(Integer.parseInt(aux[0])+1)+". La estructura IF solo acepta valores boolenos. Solucion: Verificar la condicion del IF.\n";
                     }
+
                     String val = "";
                     String[]arr = aux[3].split(" ");
                     for(int j = 2; j<arr.length-1;j++){
